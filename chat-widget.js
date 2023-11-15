1b4e9cd3c7c7aaf72b19a94c40071380e5a16ad5
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2023-11-15 12:18:35 +0400

    Add files via upload

---
+++ chat-widget.js
@@ -0,0 +1,25 @@
+window.Papercups = {
+  config: {
+    token: "b880fe29-0ee8-4919-9d74-c08cf0bd5f9a",
+    inbox: "b2b36e85-226f-4b47-ad51-9a12228a4ea0",
+    title: "Welcome to A-BET",
+    subtitle: "Ask us anything in the chat window below 😊",
+    primaryColor: "#111a33",
+    newMessagePlaceholder: "Start typing...",
+    showAgentAvailability: false,
+    agentAvailableText: "We're online right now!",
+    agentUnavailableText: "We're away at the moment.",
+    requireEmailUpfront: true,
+    iconVariant: "filled",
+    baseUrl: "https://app.papercups.io"
+    // Optionally include data about your customer here to identify them
+    // customer: {
+    //   name: __CUSTOMER__.name,
+    //   email: __CUSTOMER__.email,
+    //   external_id: __CUSTOMER__.id,
+    //   metadata: {
+    //     plan: "premium"
+    //   }
+    // }
+  },
+};
